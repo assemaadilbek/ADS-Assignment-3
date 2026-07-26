033513b2b71b18ba8f8140183c8c7dec4fcdb859
assemaadilbek <asemaadilbek3@gmail.com>
2026-07-26 20:55:35 +0500

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,1 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        if (!scanner.hasNext()) return;
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+
+        // Convert strings to character arrays
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        // Sort both arrays
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        // Compare sorted arrays
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        
+        scanner.close();
+    }
+}
